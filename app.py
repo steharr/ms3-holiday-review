c49b004f80df5376cb94eae885f361f62a8ff6e7
Stephen Harrold <steharrold@gmail.com>
2021-10-04 20:49:23 +0100

    linked up login page to back end

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import (Flask, flash, render_template,
                    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 if os.path.exists('env.py'):
@@ -41,13 +41,35 @@ def register():
             "holiday_type": request.form.get("holiday").lower()
         }
         mongo.db.users.insert_one(new_user)
+        # set the current session user
+        session["user"] = request.form.get("username").lower()
+        # flash indication
+
+        # redirect to the users profile
+        flash("Registered Successfully!")
 
     # direct to the profile page and session cookie set up for user
     return render_template("register.html", holiday_type=holiday_type)
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        # check if the username is already registered in the database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        # check if the hashed password matches the password on the database
+        if check_password_hash(existing_user["password"], request.form.get("password")):
+            # set the current sessions user
+            session["user"] = request.form.get("username").lower()
+            # flash indication
+            flash("Login Success")
+            return redirect(url_for('login'))
+        else:
+            # flash indication
+            flash("Login Failed")
+            return redirect(url_for('login'))
+
     return render_template("login.html")
 
 
