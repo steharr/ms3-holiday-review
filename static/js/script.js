ebcc589fbcea3a8804340ca978177e2a5717eadf
Stephen Harrold <steharrold@gmail.com>
2021-10-12 18:08:59 +0100

    added js for highlighting icons in write_review range inputs

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,8 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
     'use strict'
-
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     var forms = document.querySelectorAll('.needs-validation')
-
     // Loop over them and prevent submission
     Array.prototype.slice.call(forms)
         .forEach(function (form) {
@@ -18,3 +16,23 @@
             }, false)
         })
 })()
+
+
+// for cost rating slider
+function highlightIcon(num, iconType, limit) {
+    for (i = 1; i <= limit; i++) {
+        let icon = document.querySelector(`#${iconType}-${i}`)
+        let iconClasses = icon.classList;
+        if (i <= num) {
+            if (iconClasses.contains(`unfilled-${iconType}`)) {
+                icon.classList.remove(`unfilled-${iconType}`);
+            }
+            icon.classList.add(`filled-${iconType}`);
+        } else {
+            if (iconClasses.contains(`filled-${iconType}`)) {
+                icon.classList.remove(`filled-${iconType}`);
+            }
+            icon.classList.add(`unfilled-${iconType}`);
+        }
+    }
+}
